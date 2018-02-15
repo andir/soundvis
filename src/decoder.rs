8f58acf04cf8b833f0ff4c2b19cc23220312f25d
Andreas Rammhold <andreas@rammhold.de>
2018-02-15 02:30:22 +0100

    a few copy optimizations, using standard pow instead of num::num::pow

--- src/decoder.rs
+++ src/decoder.rs
@@ -42,42 +42,51 @@ pub struct Decoder {
     num_bins: usize,
     ffts: Vec<Arc<rustfft::FFT<f64>>>,
     windows: Vec<(usize, Vec<f64>)>,
-    freqs: Vec<f64>,
+    freqs: Vec<usize>,
 }
 
 impl Decoder {
-    pub fn new(
-        sample_rate: f64,
-        octaves: usize,
-        min_window: usize,
-        max_window: usize,
-    ) -> Decoder {
+    pub fn new(sample_rate: f64, octaves: usize, min_window: usize, max_window: usize) -> Decoder {
         let mut planner = rustfft::FFTplanner::new(false);
         let EASY_LEDS: f64 = 2200.0;
         let NICE_STRETCH = f64::from(EASY_LEDS * (octaves as f64) / 12.0).ceil();
-        let num_bins : usize = 12 * octaves * NICE_STRETCH as usize;
+        let num_bins: usize = 12 * octaves * NICE_STRETCH as usize;
 
-        let freqs = (0..num_bins)
+        let freqs: Vec<usize> = (0..num_bins)
             .map(|v| {
                 let v = v as f64;
-                440.0 * f64::from(num::pow::pow(2, ((v / 12.0 / NICE_STRETCH) - 3.0) as usize)) /
-                    sample_rate
+                let freq = 440.0 * 2.0_f64.powf((v / 12.0 / NICE_STRETCH) - 3.0) / sample_rate;
+                freq as usize
             })
             .collect();
         Decoder {
             sample_rate: sample_rate,
             octaves: octaves,
-            ring_buffer: vec![0 as f64; num::pow::pow(2, max_window)],
+            ring_buffer: vec![0 as f64; 2usize.pow(max_window as u32)],
             num_bins: num_bins,
             ffts: (min_window..max_window)
                 .map(|n| planner.plan_fft(num::pow::pow(2, n)))
                 .collect(),
             windows: (min_window..max_window)
                 .map(|n| {
-                    (n, apodize::hanning_iter(num::pow::pow(2, n)).collect())
+                    (n, apodize::hanning_iter(2usize.pow(n as u32)).collect())
                 })
                 .collect(),
             freqs: freqs,
+        }
+    }
+
+
+    fn _update_ring_buffer<S>(&mut self, input: &Vec<S>)
+    where
+        S: Float + ToPrimitive + FromPrimitive,
+    {
+        // shift around the ringbuffer & insert new data at end
+        self.ring_buffer.rotate_left(input.len());
+
+        let offset = self.ring_buffer.len() - input.len();
+        for (i, v) in input.iter().map(|v| v.to_f64().unwrap()).enumerate() {
+            self.ring_buffer[offset + i] = v;
         }
     }
 
@@ -85,15 +94,7 @@ impl Decoder {
     where
         S: Float + ToPrimitive + FromPrimitive,
     {
-        // shift around the ringbuffer & insert new data at end
-        {
-            self.ring_buffer.rotate_left(input.len());
-
-            let offset = self.ring_buffer.len() - input.len();
-            for (i, v) in input.iter().map(|v| v.to_f64().unwrap()).enumerate() {
-                self.ring_buffer[offset + i] = v;
-            }
-        }
+        self._update_ring_buffer(&input);
 
         // initialize one fft_out with the maximum size of samples we expect
         // we will use `element_count` to restrict the range below
@@ -105,7 +106,8 @@ impl Decoder {
         let mut fft_in = vec![c64::new(0.0, 0.0); self.ring_buffer.len()];
 
         // our "output",  we are awaiting self.num_bins bins in the output
-        let mut bins: Vec<f64> = vec![0.0; self.num_bins];
+        let mut bins: Vec<f64> = vec![0.0; self.windows.len() * self.freqs.len()];
+        let mut bins_pos : usize = 0;
 
         for (i, &(k, ref window)) in self.windows.iter().enumerate() {
             // element_count is the amount of items we should take a look at with the current window
@@ -126,7 +128,7 @@ impl Decoder {
             }
 
             // apply fft on multiple workers, the amount of jobs is equal to the amount of elements in fft_in
-            self.ffts[i].process_multi(&mut fft_in[..element_count], &mut fft_out[..element_count]);
+            self.ffts[i].process(&mut fft_in[..element_count], &mut fft_out[..element_count]);
 
             // post process fft values
             // we skip the first element (why??) and consume up to element_count elements from fft_out
@@ -147,9 +149,10 @@ impl Decoder {
                 .collect();
 
             // extract requests frequencies from tmp and add it to the bins
-            bins.extend(self.freqs.iter().map(|v| tmp[*v as usize]));
+            bins.splice(bins_pos..bins_pos+self.freqs.len(), self.freqs.iter().map(|v| tmp[*v]));
+            bins_pos += self.freqs.len();
         }
 
-        bins
+        bins[..bins_pos].to_vec()
     }
 }
